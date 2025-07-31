ead1b93ff38965a26b952acfc2897f6336d56fc0
Natanael Rio Wijaya <gafterlife@gmail.com>
2025-08-01 00:31:27 +0700

    rio

--- src/app/api/detalProduct/route.js
+++ src/app/api/detalProduct/route.js
@@ -90,7 +90,7 @@ export async function GET(req) {
         price: 50000,
         diskon: 10,
         image: `${process.env.NEXT_PUBLIC_URL}/rahasiamenujutradingbebasemosi.png`,
-        linkProduk: "https://invesdigi.com/produk/template-cv-profesional"
+        linkProduk: "https://drive.google.com/file/d/1I4XTNCSfvHU1zLC7NGWLMY7P8gQDC0Vr/view?usp=sharing"
       }
 
     ];
