4555b88a5a6614b6f2b0806e166226132a5ed70d
Natanael Rio Wijaya <gafterlife@gmail.com>
2025-08-12 22:46:53 +0700

    rio

--- src/app/api/detalProduct/route.js
+++ src/app/api/detalProduct/route.js
@@ -87,7 +87,7 @@ export async function GET(req) {
   <li> <strong>Daftar Pustaka </strong></li>
 </ul>
 `,
-        price: 10000,
+        price: 50000,
         diskon: 10,
         image: `${process.env.NEXT_PUBLIC_URL}/rahasiamenujutradingbebasemosi.png`,
         linkProduk: "https://drive.google.com/file/d/1I4XTNCSfvHU1zLC7NGWLMY7P8gQDC0Vr/view?usp=sharing"
