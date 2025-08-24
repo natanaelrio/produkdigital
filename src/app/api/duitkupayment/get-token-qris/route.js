5a985505fd199187c89ee3589bab6d910439ff04
Natanael Rio Wijaya <gafterlife@gmail.com>
2025-08-24 17:22:50 +0700

    rio

--- src/app/api/duitkupayment/get-token-qris/route.js
+++ src/app/api/duitkupayment/get-token-qris/route.js
@@ -1,70 +1,38 @@
+import { ResponseData } from "@/utils/responseData";
 import crypto from "crypto";
 
 export async function POST(req) {
     const authorization = req.headers.get('authorization')
 
     const {
-        kodeBank,
-        note,
         merchantOrderId,
         customerVaName,
-        // phoneNumber,
         email,
         itemDetails,
-        linkProduk,
     } = await req.json()
 
     const totalPrice = itemDetails.map((data) => data.price).reduce((acc, curr) => acc + curr, 0)
-    const datetime = new Date().toISOString().slice(0, 19).replace("T", " ");
     const signature = crypto
-        .createHash("sha256")
-        .update(process.env.SERVER_KODEMC + totalPrice + datetime + process.env.SERVER_KEYDUITKU)
+        .createHash("md5")
+        .update(process.env.SERVER_KODEMC + merchantOrderId + totalPrice + process.env.SERVER_KEYDUITKU)
         .digest("hex");
 
-    const headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'x-duitku-signature': signature,
-        'x-duitku-timestamp': datetime,
-        'x-duitku-merchantcode': process.env.SERVER_KODEMC
-    };
-
     const payload = {
-        merchantcode: process.env.SERVER_KODEMC,
-        amount: totalPrice,
-        datetime: datetime,
-        signature: signature,
-    };
-
-
-    console.log(process.env.SERVER_KODEMC);
-    console.log(datetime);
-    console.log(process.env.SERVER_KEYDUITKU);
-    console.log(totalPrice);
-    console.log(signature);
-
-
-
-    const bodynya = {
+        "merchantCode": process.env.SERVER_KODEMC,
         "paymentAmount": itemDetails.map((data) => data.price).reduce((acc, curr) => acc + curr, 0),
-        "merchantOrderId": merchantOrderId,
-        "productDetails": note ? note : 'tidak ada catatan',
-        "additionalParam": JSON.stringify({
-            email: email,
-            name: customerVaName,
-            linkProduk: linkProduk
-        }),
-        "customerVaName": customerVaName,
-        "email": email,
-        // "phoneNumber": phoneNumber,
-        "itemDetails": itemDetails,
+        "paymentMethod": "SP", // contoh: "VC", "QRIS", "VA"
+        merchantOrderId,
+        "productDetails": 'InvesDigi',
+        customerVaName: customerVaName,
+        email,
+        itemDetails: itemDetails,
         "callbackUrl": `${process.env.NEXT_PUBLIC_URL}/api/duitkupayment`,
         "returnUrl": `${process.env.NEXT_PUBLIC_URL}`,
-        "expiryPeriod": 60,
-        "paymentMethod": "PS"
-    }
+        signature,
+        expiryPeriod: 10,
+    };
 
-    const resDuitku = await fetch("https://passport.duitku.com/webapi/api/merchant/paymentmethod/getpaymentmethod", {
+    const resDuitku = await fetch(process.env.NEXT_PUBLIC_QRIS, {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
@@ -73,10 +41,6 @@ export async function POST(req) {
     })
 
     const data = await resDuitku.json()
-    return new Response(JSON.stringify({ isCreated: true, data: data }), {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-    });
-    // const res = await ResponseData(data, authorization)
+    const res = await ResponseData(data, authorization)
     return res
 }
